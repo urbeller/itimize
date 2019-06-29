9bda6d654a68d8c665b43bd24e7943fd8f626425
Jamil Drareni <urbeller@macbookpro>
2019-06-29 18:15:51 -0400

    fix tabs.

--- include/itimize.h
+++ include/itimize.h
@@ -1,6 +1,6 @@
 #ifndef __ITIMIZE_H__
 #define __ITIMIZE_H__
-
+  
 #include <iostream>
 #include <ctime>
 #include <chrono>
@@ -20,72 +20,72 @@
 
 namespace itimize
 {
-	using TimePoint = std::chrono::time_point<std::chrono::high_resolution_clock>;
-	using Duration = double; // in ms.
+  using TimePoint = std::chrono::time_point<std::chrono::high_resolution_clock>;
+  using Duration = double; // in ms.
 
-	TimePoint Now()
-	{
-		return std::chrono::high_resolution_clock::now();
-	}
+  TimePoint Now()
+  {
+    return std::chrono::high_resolution_clock::now();
+  }
 
-	struct TimingEntry
-	{
-			TimingEntry(const std::string &l) : label_(l) {}
-			TimingEntry(const std::string &l, const TimePoint &s) : label_(l), start_(s){}
+  struct TimingEntry
+  {
+      TimingEntry(const std::string &l) : label_(l) {}
+      TimingEntry(const std::string &l, const TimePoint &s) : label_(l), start_(s){}
 
-			void compute_duration()
-			{ 
-				std::chrono::duration<double, std::milli> duration = end_ - start_;
-				duration_ = duration.count();
-			}
+      void compute_duration()
+      { 
+        std::chrono::duration<double, std::milli> duration = end_ - start_;
+        duration_ = duration.count();
+      }
 
-			std::string label_;
-			TimePoint start_;
-			TimePoint end_;
-			Duration duration_;
-	};
+      std::string label_;
+      TimePoint start_;
+      TimePoint end_;
+      Duration duration_;
+  };
 
-	class Ticker
-	{
-		public:
-			Ticker(const Ticker&) = delete;
+  class Ticker
+  {
+    public:
+      Ticker(const Ticker&) = delete;
 
-		 	Ticker(const std::string &label) : entry_(label)
-			{ 
-				entry_.start_ = Now();
-			}
+      Ticker(const std::string &label) : entry_(label)
+      { 
+        entry_.start_ = Now();
+      }
 
-			~Ticker()
-			{
-				entry_.end_ = Now();
-				entry_.compute_duration();
-				std::cout << entry_.label_ << " " << entry_.duration_ << std::endl;
-			}
+      ~Ticker()
+      {
+        entry_.end_ = Now();
+        entry_.compute_duration();
+        std::cout << entry_.label_ << " " << entry_.duration_ << std::endl;
+      }
 
-		private:
-			TimingEntry entry_;
-	};
+    private:
+      TimingEntry entry_;
+  };
 
-	class TimeCentral
-	{
-		public:
-			static TimeCentral* getInstance()
-			{
-				if(!instance)
-					instance = new TimeCentral();
+  class TimeCentral
+  {
+    public:
+      static TimeCentral* getInstance()
+      {
+        if(!instance)
+          instance = new TimeCentral();
 
-				return instance;
-			}
+        return instance;
+      }
 
-		private:
-			TimeCentral(){}
-			
-			std::map<std::string, TimingEntry> records;
+    private:
+      TimeCentral(){}
+      
+      std::map<std::string, TimingEntry> records;
 
-			static TimeCentral* instance;
-	};
+      static TimeCentral* instance;
+  };
 
-	TimeCentral *TimeCentral::instance = nullptr;
+  TimeCentral *TimeCentral::instance = nullptr;
 
 };
 
