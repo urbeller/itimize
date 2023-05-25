20053c4ae663c147db3b0d09553b15113be1b09a
Jamil Drareni <urbeller@gmail.com>
2023-05-25 12:01:09 -0400

    updated the example to demo the new TimeCenter capabilities.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,6 +1,7 @@
 #include "itimize.h"
 #include <iomanip>
 #include <cmath>
+#include <thread>
 
 using namespace std;
 using namespace itimize;
@@ -40,10 +41,21 @@ double func2()
   return r;
 }
 
+void func3(int sec)
+{
+
+  // This will add an entry for "func3".
+  // NOTE: START_TIMER must be matched with an
+  // END_TIMER.
+  START_TIMER("func3");
+    std::this_thread::sleep_for(std::chrono::seconds(sec));
+  END_TIMER("func3");
+}
+
 int main(int , char **)
 {
 
-  TIME_NAMED_BLOCK("Main function: ");
+  TIME_NAMED_BLOCK("Main function");
 
   // Each of these functions call TIME_BLOCK().
   double res1 = func1();
@@ -54,8 +66,31 @@ int main(int , char **)
   // recusrive call will create a Ticker object!!
   {
     TIME_NAMED_BLOCK("line " STR1(__LINE__));
-    int res3 = fibonacci(45);
+    int res3 = fibonacci(15);
   }
+
+
+  // Show how to use the TimeCenter to log
+  // multi-point timers.
+  for(int i = 0; i < 3; ++i)
+  {
+    func3(i);
+  }
+
+  // Print timer stats (default unit is milli-secs). The format is:
+  // [label_of_timer(# of instances)]: total_time avg-min-max=average/min/max times.
+  PRINT_TIMER("func3");
+  
+  // Remove entries of labeled timer from TimeCenter.
+  RESET_TIMER("func3");
+
+  // Timer printing support th following units:
+  // "s" for seconds, "ms" for millisecs (default), "us" for micro-secs.
+  func3(1);
+  func3(2);
+  PRINT_TIMER("func3","s");
+  PRINT_TIMER("func3","ms");
+  PRINT_TIMER("func3","us");
 
 	return 0;
 }
